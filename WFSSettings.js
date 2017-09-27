3d399f9dc104c25e3a13dadbb66d249485ce1136
Marc J Cenac <mcenac@boundlessgeo.com>
2017-09-27 10:00:17 -0500

    adding activityindicator when syncing submissions

--- WFSSettings.js
+++ WFSSettings.js
@@ -1,13 +1,37 @@
 import React, { Component } from 'react';
-import { Button, Text, View } from 'react-native';
+import { Button, Text, View, ActivityIndicator, StyleSheet, Modal, Platform } from 'react-native';
 import * as db from './db';
 import { blue, orange, gray, darkGray } from './styles';
 
+
+const styles = StyleSheet.create({
+  modalContainer: {
+      backgroundColor: 'rgba(0, 0, 0, 0.4)',
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    modal: {
+      backgroundColor: 'white',
+      width: 100,
+      height: 100,
+      alignItems: 'center',
+      justifyContent: 'center',
+      borderRadius: Platform.OS === 'ios' ? 10 : 2,
+    },
+});
+
 export default class WFSSettings extends Component {
 
-  syncSubmissions = (wfs) => {
-    // todo: start a spinner
-    db.syncWFS(wfs);
+  constructor(props) {
+    super(props);
+    this.state = { syncing: false };
+  }
+
+  syncSubmissions = async wfs => {
+    this.setState({ syncing: true });
+    await db.syncWFS(wfs);
+    this.setState({ syncing: false });
   };
 
   render() {
@@ -35,6 +59,13 @@ export default class WFSSettings extends Component {
           <Button title='Sync' onPress={() => this.syncSubmissions(wfs)}/>
           <Button onPress={() => db.refreshWFS(this.props.navigation.state.params.wfs)} title="Refresh" />
         </View>
+        <Modal visible={this.state.syncing} transparent>
+          <View style={styles.modalContainer}>
+            <View style={styles.modal}>
+              <ActivityIndicator size='large' />
+            </View>
+          </View>
+        </Modal>
       </View>
     );
   }
