1a4453da98929126ac5699b0efd5db80afc18ed8
Frank Rowe <fsrowe@gmail.com>
2017-09-29 10:41:43 -0400

    increase tap area

--- FAnnotation.js
+++ FAnnotation.js
@@ -7,20 +7,35 @@ const DEFAULT_BORDER_WIDTH = 2;
 const DEFAULT_BORDER_COLOR = '#333';
 const DEFAULT_COLOR = 'rgba(255,65,54,0.8)';
 
-export const FAnnotationView = ({ radius, borderWidth, backgroundColor, selected }) => (
-  <View
-    style={{
-      width: selected ? radius + DEFAULT_BORDER_WIDTH : radius,
-      height: selected ? radius + DEFAULT_BORDER_WIDTH : radius,
-      borderWidth: selected ? DEFAULT_BORDER_WIDTH : 0,
-      borderColor: DEFAULT_BORDER_COLOR,
-      borderRadius: (selected ? radius + DEFAULT_BORDER_WIDTH : radius) / 2,
-      backgroundColor: backgroundColor,
-      justifyContent: 'center',
-      alignItems: 'center',
-    }}
-  />
-);
+export const FAnnotationView = ({ radius, backgroundColor, selected }) => {
+  const width = selected ? radius + DEFAULT_BORDER_WIDTH : radius;
+  const height = width;
+  const borderWidth = selected ? DEFAULT_BORDER_WIDTH : 0;
+  const borderRadius = width / 2;
+  return (
+    <View
+      style={{
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: width + 10,
+        height: height + 10,
+      }}
+    >
+      <View
+        style={{
+          width,
+          height,
+          borderWidth,
+          borderColor: DEFAULT_BORDER_COLOR,
+          borderRadius,
+          backgroundColor: backgroundColor,
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      />
+    </View>
+  );
+};
 
 const FAnnotation = ({ feature, onOpenAnnotation, backgroundColor, radius, selected }) => {
   let _radius = radius ? radius : DEFAULT_RADIUS;
